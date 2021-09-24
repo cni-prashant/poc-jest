be18a839878a4cfe2ef54a0631043a0015cb155a
Prashant Singh <psingh@cnicorp.local>
2021-09-24 17:35:43 +0530

    changes added

--- graphql/queries/generic.js
+++ graphql/queries/generic.js
@@ -10,5 +10,13 @@ verifyAccount: (name, password) => {
       }
     }
   };
-}
+},
+switchAuthContext: input => {
+  return {
+    operationName: 'switchAuthContext',
+    query:
+      'mutation switchAuthContext($refreshToken: String!, $role: SystemRoleCode, $artistId: Int, $organizationId: Int) {  switchAuthContext(refreshToken: $refreshToken, role: $role, artistId: $artistId, organizationId: $organizationId) {    access    refresh    __typename  }}',
+    variables: input
+  };
+}, 
 }
--- tests/login.test.js
+++ tests/login.test.js
@@ -17,12 +17,12 @@ test('login into CD with invalid credentials', async () => {
   expect(res.body.errors[0].message).toBe("We couldn't find an account associated with that email or username.");
 });
 
-// test('verify basic filters functionality', async () => {
-//   //login into account
-//   let res = await helpers.getResponse(login.verifyAccount(cred.cdUser, cred.password));
-//   expect(res.status).toBe(200);
-//   expect(res.body.data.verifyAccount.token.access).not.toBe(null);
-//   let accessToken = res.body.data.verifyAccount.token.access;
+test('verify basic filters functionality', async () => {
+  //login into account
+  let res = await helpers.getResponse(login.verifyAccount(cred.cdUser, cred.password));
+  expect(res.status).toBe(200);
+  expect(res.body.data.verifyAccount.token.access).not.toBe(null);
+  let accessToken = res.body.data.verifyAccount.token.access;
 
-//   let res = await helpers.getResponseWithAuthToken(accessToken, )
-// });
+  let res = await helpers.getResponseWithAuthToken(accessToken, )
+});
