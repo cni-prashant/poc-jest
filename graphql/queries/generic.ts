471ebe90085545b1226698d1eb102eadacc6a03e
ko4 <kochergajob@gmail.com>
2021-09-27 19:10:09 +0300

    ts

---
+++ graphql/queries/generic.ts
@@ -0,0 +1,22 @@
+export function verifyAccount(name: string, password: string) {
+  return {
+    operationName: 'verifyAccount',
+    query:
+      'query verifyAccount($input: VerifyAccountInput!) {verifyAccount(input: $input)}',
+    variables: {
+      input: {
+        name,
+        password,
+      },
+    },
+  };
+}
+
+export function myAccount() {
+  return {
+    operationName: 'myAccount',
+    query:
+      'query myAccount {  myAccount {    id    firstName    lastName    countryId    email    isAdmin    country {      id      code      name      continent {        id        __typename      }      unitSystem {        id        code        __typename      }      __typename    }    artists {      minor      artistId      created      profiles {        profileId        stageName        dateOfBirth        created        isPersonal        isPlayableAgeLockedToTalent        profileStatus {          id          code          name          __typename        }        __typename      }      __typename    }    isIPC    organizations {      id      features {        id        code        name        enabled        __typename      }      __typename    }    divisions {      id      name      logo {        id        fileKey        guid        mediaType {          id          code          __typename        }        mediaStatus {          id          code          __typename        }        __typename      }      isOrganization      parentOrganization {        id        name        logo {          id          fileKey          guid          __typename        }        __typename      }      __typename    }    timeZone {      id      code      standardName      __typename    }    __typename  }}',
+    variables: {},
+  };
+}
---
+++ tests/login.test.ts
@@ -0,0 +1,25 @@
+import * as login from '../graphql/queries/generic';
+import * as helpers from '../helpers';
+import * as cred from '../config/credentials.json';
+
+describe('Casting Director, login', () => {
+  test('Login with valid credentials', async () => {
+    const res = await helpers.getResponse(
+      login.verifyAccount(cred.cdUser, cred.password),
+    );
+
+    expect(res.status).toBe(200);
+    expect(res.body.data.verifyAccount.token.access).not.toBe(null);
+  });
+
+  test('Login with invalid credentials', async () => {
+    const res = await helpers.getResponse(
+      login.verifyAccount(cred.invalidCDUser, cred.password),
+    );
+
+    expect(res.status).toBe(200);
+    expect(res.body.errors[0].message, 'QAAQAQAQ').toBe(
+      "We couldn't find an account associated with that email or username PPP",
+    );
+  });
+});
